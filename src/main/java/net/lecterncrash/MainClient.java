504d84b9b5daab6634ba7ca8b95eeeef8ebb5331
Coderx-Gamer <85349822+Coderx-Gamer@users.noreply.github.com>
2022-04-04 06:18:21 -0400

    Add files via upload

---
+++ src/main/java/net/lecterncrash/MainClient.java
@@ -0,0 +1,9 @@
+package net.lecterncrash;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MainClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
